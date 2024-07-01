d83e936b3316760d2d8d8bc7e0ac73fb9dbc321b
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-06-30 20:34:03 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string)
+{
+    
+    return string.toUpperCase("hello");
+}
+;
+function whisper(string) {
+  return string.toLowerCase("Goodbye")
+}
+function logShout(string) {
+console.log(("hello").toUpperCase("hello"))
+return string.toUpperCase("hello");
+}
+function logWhisper(string) {
+    console.log(("HELLO").toLowerCase("HELLO"))
+    return string.toLowerCase("HELLO")
+}
+
+function sayHiToHeadphonedRoommate(string)
+ {
+if (string === ("Let's have dinner together!")) {
+return ("I would love to!")
+} else if (string !== string.toUpperCase("")) {
+return ("I can't hear you!")
+} else if (string === string.toUpperCase("")) {
+  return ("YES INDEED!")
+}}
+console.log(sayHiToHeadphonedRoommate("HELLO!"))
